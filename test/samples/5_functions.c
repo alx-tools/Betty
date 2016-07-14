426cb41a42612ed685d360e0c21015d475627d3f
Alexandre Gautier <alexandre.gautier@epitech.eu>
2016-07-14 00:04:50 -0700

    5 functions check

---
+++ test/samples/5_functions.c
@@ -0,0 +1,25 @@
+/*
+* Only one function not indented
+*/
+void function(void)
+{
+if(something)
+{
+if(something)
+{
+if(something)
+{
+if(something)
+{
+if(something)
+{
+if(something)
+{
+printf("Hey!\n");
+}
+}
+}
+}
+}
+}
+}
