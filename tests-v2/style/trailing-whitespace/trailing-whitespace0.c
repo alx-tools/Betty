d023b0a89695eaa5a4fe5e381b787e9c52b60b18
Alexandre Gautier <alexandre.gautier@epitech.eu>
2017-02-02 21:38:16 +0300

    Started tests suits for V2

---
+++ tests-v2/style/trailing-whitespace/trailing-whitespace0.c
@@ -0,0 +1,9 @@
+/*
+ * Be careful if you save this file from a text editor
+ * Be sure not to delete intentional whitespaces for tests purpose
+ */
+
+int main(void)
+{     
+	return (0);    
+}
---
+++ tests-v2/style/trailing-whitespace/trailing-whitespace1.h
@@ -0,0 +1,6 @@
+#ifndef _TEST_H_
+#define _TEST_H_    
+
+int main(void);    
+
+#endif /* _TEST_H_ */  
